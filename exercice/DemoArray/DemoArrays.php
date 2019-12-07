83dcff5b72f58775403d4136f12befbbb0283b11
EGiorgisCPNV <48352389+EGiorgisCPNV@users.noreply.github.com>
2019-12-07 13:33:09 +0100

    Fin de l'exercice Introduction aux tableaux

--- exercice/DemoArray/DemoArrays.php
+++ exercice/DemoArray/DemoArrays.php
@@ -19,9 +19,7 @@ $browsers = array('Safari','Edge','Chrome','Firefox');
 $mostUsedBrowser = $browsers[2];
 
 // Replaces 'Edge' with 'Opera'
-$browsers[1]='Oper';
-
-
+$browsers[1]='Opera';
 
 
 
@@ -30,12 +28,12 @@ $browsers[1]='Oper';
 // Loop through an Indexed Array
 $arrLength = count($browsers);// Assign the number of Elements of the Array '$browsers'
 
-for($i=0;$i<4;$i++) {
 
-    foreach ($browsers[$i] as $browsersList) {
-        $browsersList = "Safari - Opera - Chrome - Firefox";
-    }
-    echo $browsers[$i];
+$browsersList = "";//assigne une valeur de base a $browsersList
+
+//tant que toute les valeur du tableau $browsers n'auront pas été prise la boucle for ne s'arretera pas
+for ($index = 0; $index < $arrLength; $index++) {
+        $browsersList =  $browsersList.$browsers[$index].' - ';//ajoute un tiret après chaque mot
 }
 
 
@@ -54,16 +52,27 @@ $contactInfo= array(
 );
 
 // assign the city ("Yverdon") to $Home
-//TODO - il vous faut coder ici
+$Home=$contactInfo['City'];
+
+
+
 
 // Replaces 'John Doe' with 'Jane Doe'
 //TODO - il vous faut coder ici
+$contactInfo['name']='Jane Doe';//remplace la valeur de name par Jane Doe
+
+
 
 // Loop through an Associated Array
-$completedContact="";
+$completedContact="";//assigne une valeur de base a $completedContact
+
+//tant qu'il na pas parcouru tout les index du tableau $contactInfo il continura a ajouter en string chaque index du tableau $contactInfo dans la variable $completedContact
 foreach ($contactInfo as $element){
-    //TODO - il vous faut coder ici
+    $completedContact=$completedContact.$element." - ";//ajoute un tiret après chaque mot
 }
+
+
+
 //</editor-fold>
 
 //<editor-fold desc="Two-dimensional Array">
@@ -76,9 +85,14 @@ $people= array(
 
 // assign "Joe" to $person
 //TODO - il vous faut coder ici
+$person=$people[2][0];
+
+
+
 
 // Replaces 'Bart' with 'Lisa'
 //TODO - il vous faut coder ici
+$people[1][2]='Lisa';
 
 
 // Loop through an Two-dimensional Array
@@ -87,4 +101,16 @@ $colLength = count($people[0]);     //Assign the number of Elements (columns) of
 $personList="";                     // Initialise the string $PersonList
 
 //TODO - il vous faut coder ici
+
+//tant qu'il n'aura pas prit toute les ligne du tableau $people il ne s'aretera pas
+for($i=0;$i<$rowLength;$i++){
+
+    //tant qu'il n'aura pas prit chaque valeur d'une ligne du tableau $people il ne s'aretera pas
+    for( $j=0;$j<$colLength;$j++){
+        $personList=$personList.$people[$i][$j].' - ';//$personList va est egale a chaque valeur d'une ligne du tableau $people
+    }
+
+}
+
+
 //</editor-fold>

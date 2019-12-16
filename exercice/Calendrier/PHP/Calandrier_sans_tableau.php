f03c87633bc2e74f71bf49075307237b7b2f258b
EGiorgisCPNV <48352389+EGiorgisCPNV@users.noreply.github.com>
2019-12-16 13:50:28 +0100

    Continuation de l'exercice calendrier

--- exercice/Calendrier/PHP/Calandrier_sans_tableau.php
+++ exercice/Calendrier/PHP/Calandrier_sans_tableau.php
@@ -26,9 +26,24 @@
         //Tableau des mois
         $tableauMois = array('Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre');
 
-        //affiche le mois en farancais par rapport a l'index du tableau : $tableauMois
-        echo $tableauMois[date("m") - 1] . "<br>";
-        echo date("o");
+
+        if (isset($_POST['test']))
+        {
+            $result=$_POST['test'];
+            echo $result . "<br>";
+            echo date("o");
+        }
+        else
+        {
+            $result=$tableauMois[date("m") - 1];
+        }
+
+        ?>
+
+        <?php
+
+        //affiche le mois en francais par rapport a l'index du tableau : $tableauMois
+
         ?>
     </ul>
 </div>
@@ -55,37 +70,65 @@
 </ul>
 
 <!-- affiche les jours en chiffre-->
+
 <ul class="days">
     <?php
+    if (isset($_POST['test']))
+    {
 
-    //tableau des jours en nombre
-    $tableauNumJour = array();
+        //////////////////////////////////////////////////////////////////////////////
+        //tableau des jours en nombre
+        $tableauNumJour = array();
 
-
-    //boucle for qui aligne le jour avec le bon chiffre
-    for($w=1;$w < 8-date("N");$w++){
-        echo"<li></li>";
-    }
-
-    //boucle qui va afficher les 31 jours et qui va mettre en valeur le jour actuelle
-    for ($a = 1; $a < 31; $a++) {
-
-        $tableauNumJour[$a] = $a;
-
-
-        //condition qui va mettre en valeur le jour actuelle
-        if ($a == date("d")) {
-            echo '<li><span class="active">' . $tableauNumJour[$a] . '</span>';
-        } else {
-            echo '<li>';
-            echo $tableauNumJour[$a];
-
+        //boucle for qui aligne le jour avec le bon chiffre
+        for($w=1;$w < 8-date("N");$w++){
+            echo"<li></li>";
         }
+
+
+        //boucle qui va afficher les 31 jours et qui va mettre en valeur le jour actuelle
+        for ($a = 1; $a < date("t"); $a++) {
+
+            $tableauNumJour[$a] = $a;
+
+            //condition qui va mettre en valeur le jour actuelle
+            if ($a == date("d")) {
+                echo '<li><span class="active">' . $tableauNumJour[$a] . '</span>';
+            } else {
+                echo '<li>';
+                echo $tableauNumJour[$a];
+
+            }
+        }
+        //////////////////////////////////////////////////////////////////////////////////////
+    }
+    else
+    {
+        $result=$tableauMois[date("m") - 1];
     }
 
 
     ?>
 </ul>
+<form method="POST" action="/PHP/Calandrier_sans_tableau.php">
+
+    <select name="test">
+        <option><?= $tableauMois[0]?></option>
+        <option><?= $tableauMois[1]?></option>
+        <option><?= $tableauMois[2]?></option>
+        <option><?= $tableauMois[3]?></option>
+        <option><?= $tableauMois[4]?></option>
+        <option><?= $tableauMois[5]?></option>
+        <option><?= $tableauMois[6]?></option>
+        <option><?= $tableauMois[7]?></option>
+        <option><?= $tableauMois[8]?></option>
+        <option><?= $tableauMois[9]?></option>
+        <option><?= $tableauMois[10]?></option>
+        <option><?= $tableauMois[11]?></option>
+    </select>
+
+    <input type="submit">
+</form>
 
 </body>
 </html>
